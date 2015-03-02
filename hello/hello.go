05984011cd72012a1d00f20ea20266db934a14f3
adnaydenov <angel.dimitrov.naydenov@gmail.com>
2015-03-02 13:14:23 +0200

    Final version of my first Go program.

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/adnaydenov/golang-projects/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
